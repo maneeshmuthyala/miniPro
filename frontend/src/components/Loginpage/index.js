b41a86e817285a748c4de2ded45ac86a02bc6d3f
maneeshmuthyala <maneeshmuthyala10@gmail.com>
2025-03-24 10:30:32 +0530

    Pictures

--- frontend/src/components/Loginpage/index.js
+++ frontend/src/components/Loginpage/index.js
@@ -135,11 +135,7 @@ class Loginpage extends Component {
           alt="website login"
         />
         <form className="form-container" onSubmit={this.submitForm}>
-          <img
-            src="https://s3-alpha-sig.figma.com/img/a7c2/c95a/e57df5f03ddceb5a4011eb1efd953170?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=i7lvM1s0i74MfggIel4C7VrexlGfffkxShALGiS99Txtnir6kf9cE0wDtpfwUiHXVw0BRZQHQIfzxZw1EQ0H1ClfCtFjay3rQCAIS~nSYjLOjg2TBlpcxwCRke5MQCppl-mEDFaqK3aVya7rCuiCkN2hntF9bpWkY8IHJUAJHnAT3XawIK1KXfQdPSW8NrdCJX0OPs0WWHm1JoUY~g374aVLigYQeWKFPHWCLbp7LdkPNRoL~08fbV~qj1viBUfox9ktE9IfCzq6zwxjitIwcAaYxJhH7o77FBChfuYVChd6aYDxaStNv9~tWtzOPwKUir3zVend3~SPe9Jc-FP5VA__"
-            className="login-website-logo-desktop-img"
-            alt="website logo"
-          />
+         <h1>Uniq</h1>
           <div className="input-container">{this.renderUsernameField()}</div>
           <div className="input-container">{this.renderPasswordField()}</div>
           <div className="bt-co">
